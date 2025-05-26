2be650d96585a8c383212b3130b99e83584fb13c
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2025-05-26 00:19:37 -0400

    bounds and speed scaling applied to top-down

--- Assets/Scripts/Player and Camera Controls/CameraController.cs
+++ Assets/Scripts/Player and Camera Controls/CameraController.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -21,9 +23,10 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float minimumHeight = 1;  // placeholder values that will get programatically decided
     [SerializeField] private float maximumHeight = 100; // either by an algorithm per the map generation, or
     [SerializeField] private float currentHeight = 5;  // through some other heuristic
+    [SerializeField] private Bounds2D boundary;
 
     public float zoomSpeed = 10;
-    public float planeMoveSpeed = 10;
+    public float planeMoveBaseSpeed = 10;
 
     [Header("FPS View Controls")]
     [SerializeField] private GameObject _cameraOrientation;
@@ -70,7 +73,7 @@ public class CameraController : MonoBehaviour
     }
 
     // TOP-DOWN/BIRDS
-    
+
 
     private void SwitchToTopDown()
     {
@@ -91,7 +94,27 @@ public class CameraController : MonoBehaviour
 
         // WASD movement
         Vector3 moveXY = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        transform.position += moveXY * planeMoveSpeed * Time.deltaTime;
+        float heightSpeedScale = Mathf.Sqrt(currentHeight - minimumHeight + 1f);
+        transform.position += heightSpeedScale * planeMoveBaseSpeed * Time.deltaTime * moveXY;
+
+        // Bounds
+        float newX = Mathf.Clamp(transform.position.x, boundary.minX, boundary.maxX);
+        float newZ = Mathf.Clamp(transform.position.z, boundary.minZ, boundary.maxZ);
+
+        transform.position = new Vector3(newX, currentHeight, newZ);
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Vector3 bottomLeft = new Vector3(boundary.minX, transform.position.y, boundary.minZ);
+        Vector3 bottomRight = new Vector3(boundary.maxX, transform.position.y, boundary.minZ);
+        Vector3 topLeft = new Vector3(boundary.minX, transform.position.y, boundary.maxZ);
+        Vector3 topRight = new Vector3(boundary.maxX, transform.position.y, boundary.maxZ);
+
+        Gizmos.DrawLine(bottomLeft, bottomRight);
+        Gizmos.DrawLine(bottomRight, topRight);
+        Gizmos.DrawLine(topRight, topLeft);
+        Gizmos.DrawLine(topLeft, bottomLeft);
     }
 
     // FIRST-PERSON
@@ -131,7 +154,7 @@ public class CameraController : MonoBehaviour
         inputLagTimer += Time.deltaTime;
         Vector2 input = PlayerInputHandler.Instance.LookInput;
 
-        if((Mathf.Approximately(0, input.x) && Mathf.Approximately(0, input.y)) == false || inputLagTimer >= inputLagPeriod)
+        if ((Mathf.Approximately(0, input.x) && Mathf.Approximately(0, input.y)) == false || inputLagTimer >= inputLagPeriod)
         {
             lastInputEvent = input;
             inputLagTimer = 0;
