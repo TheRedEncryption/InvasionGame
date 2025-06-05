afc6646071075a166e326db6c958e4d457f548cd
Zachary Cardwell <131729898+ZRedCon@users.noreply.github.com>
2025-06-04 20:02:21 -0400

    Added Entity class

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 [RequireComponent(typeof(StateMachine), typeof(Rigidbody))]
 public class Entity : MonoBehaviour
 {
-    public CappedFloat Health;
+    public CappedFloat _health;
+    [HideInInspector] public StateMachine _stateMachine;
+    [HideInInspector] public Rigidbody _rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody>();
+        _stateMachine = GetComponent<StateMachine>();
+        _stateMachine["Host"] = this;
+        
+        _health.OnEmpty += OnDeath;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public virtual void TakeDamage(float amount)
+    {
+        _health -= amount;
+    }
+
+    public virtual void Regenerate(float amount)
+    {
+        _health += amount;
+    }
+
+    public virtual void OnDeath()
+    {
         
     }
+    
 }
