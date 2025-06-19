397ffb067b1a71c74fd05f4e6495153a2dda96b2
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2025-06-19 11:26:47 -0400

    made toggleable build menu

---
+++ Assets/Scripts/UI/ToggleBuildMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleBuildMenu : MonoBehaviour
+{
+
+    [SerializeField] private Toggle toggle;
+    [SerializeField] private GameObject buildingPanel;
+
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(OnToggleChanged);
+    }
+
+    void OnToggleChanged(bool toggleValue)
+    {
+        buildingPanel.SetActive(toggleValue);
+    }
+}
