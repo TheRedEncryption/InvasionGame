af0902548e440ba285e4a2139762d289688588e0
Zachary Cardwell <131729898+ZRedCon@users.noreply.github.com>
2025-06-23 17:39:50 -0400

    GridDrawer fixed

--- Assets/Scripts/Editor/GridDrawer.cs
+++ Assets/Scripts/Editor/GridDrawer.cs
@@ -60,59 +60,62 @@ public class GridDrawer : PropertyDrawer
     }
 }
 
-
-
-// Extension method to get the parent object of a SerializedProperty
-public static class SerializedPropertyExtensions
+[CustomPropertyDrawer(typeof(PlacementGrid))]
+public class PlacementGridDrawer : PropertyDrawer
 {
-    public static object GetParentObject(this SerializedProperty property)
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        string path = property.propertyPath.Replace(".Array.data[", "[");
-        object obj = property.serializedObject.targetObject;
-        string[] elements = path.Split('.');
+        PlacementGrid target = fieldInfo.GetValue(property.GetParentObject()) as PlacementGrid;
 
-        foreach (var element in elements)
+        float lineHeight = EditorGUIUtility.singleLineHeight;
+        float spacing = 2f;
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Draw label
+        position = EditorGUI.PrefixLabel(position, label);
+
+        // Split line into four columns: Label1, Field1, Label2, Field2
+        float labelWidth = 45f;
+        float fieldWidth = position.width - EditorGUIUtility.fieldWidth + 5;
+
+        Rect row1 = new(position.x, position.y, position.width, lineHeight);
+        Rect row2 = new(position.x, position.y + lineHeight + spacing, position.width, lineHeight);
+
+        Rect scaleLabel = new(row1.x, row1.y, labelWidth, lineHeight);
+        Rect scaleField = new(scaleLabel.xMax + 2, row1.y, fieldWidth, lineHeight);
+
+        Rect dimensionsLabel = new(row2.x, row2.y, labelWidth, lineHeight);
+        Rect dimensionsField = new(scaleLabel.xMax + 2, row2.y, fieldWidth, lineHeight);
+
+        EditorGUI.LabelField(scaleLabel, "Scale");
+        float scaleFactor = EditorGUI.FloatField(scaleField, GUIContent.none, target.Scale.x);
+        target.Scale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+        EditorGUI.LabelField(dimensionsLabel, "Dimensions");
+        Vector3Int bridge = EditorGUI.Vector3IntField(dimensionsField, GUIContent.none, new Vector3Int(target.Dimensions.X, target.Dimensions.Y, target.Dimensions.Z));
+        target.Dimensions = (Grid.Point)new Vector3(Mathf.Max(bridge.x, 1), Mathf.Max(bridge.y, 1), Mathf.Max(1, bridge.z));
+
+        // Check if the property changed
+        if (EditorGUI.EndChangeCheck())
         {
-            if (element.Contains("["))
-            {
-                string elementName = element.Substring(0, element.IndexOf("["));
-                int index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
-                obj = GetValue(obj, elementName, index);
-            }
-            else
-            {
-                obj = GetValue(obj, element);
-            }
+            // Get the target object
+            GameObject targetObj = (property.serializedObject.targetObject as MonoBehaviour).gameObject;
+
+            // Mark the GameObject as dirty
+            EditorUtility.SetDirty(targetObj);
+
+            // Repaint the Scene view
+            SceneView.RepaintAll();
         }
-        return obj;
+
+        EditorGUI.EndProperty();
     }
 
-    private static object GetValue(object source, string name)
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        if (source == null)
-            return null;
-        var type = source.GetType();
-        var f = type.GetField(name, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
-        if (f == null)
-        {
-            var p = type.GetProperty(name, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.IgnoreCase);
-            if (p == null)
-                return null;
-            return p.GetValue(source, null);
-        }
-        return f.GetValue(source);
-    }
-
-    private static object GetValue(object source, string name, int index)
-    {
-        var enumerable = GetValue(source, name) as System.Collections.IEnumerable;
-        if (enumerable == null) return null;
-        var enm = enumerable.GetEnumerator();
-        for (int i = 0; i <= index; i++)
-        {
-            if (!enm.MoveNext()) return null;
-        }
-        return enm.Current;
+        return (EditorGUIUtility.singleLineHeight + 2f) * 2;
     }
 }
+
 #endif
